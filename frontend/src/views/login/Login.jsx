5f08c62542343301f5830df48187ada5a7579245
SalahTazoukanit <salahtazu1996@gmail.com>
2024-09-05 12:02:43 +0200

    register && login done

--- frontend/src/views/login/Login.jsx
+++ frontend/src/views/login/Login.jsx
@@ -1,4 +1,59 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
-  return <></>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const login = (e) => {
+    e.preventDefault();
+
+    const user = {
+      email: email,
+      password: password,
+    };
+
+    axios
+      .post("http://127.0.0.1:8000/api/v1/users/login", user)
+      .then((response) => {
+        console.log(response);
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+        alert(response.data.message);
+        navigate("/");
+      });
+  };
+  return (
+    <>
+      <div>
+        <div>
+          <h1>Se connecter</h1>
+        </div>
+        <form onSubmit={(e) => login(e)}>
+          <div>
+            <label htmlFor="">
+              Email :
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                name="email"
+              />
+            </label>
+            <label htmlFor="">
+              Password :
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                name="password"
+              />
+            </label>
+          </div>
+          <button type="submit">Se Connecter</button>
+        </form>
+      </div>
+    </>
+  );
 };
 export default Login;
--- frontend/src/views/register/Register.jsx
+++ frontend/src/views/register/Register.jsx
@@ -1,18 +1,75 @@
+import axios from "axios";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [password_confirmation, setPasswordConfirmation] = useState("");
+
+  const registerUser = (e) => {
+    e.preventDefault();
+    const user = {
+      name: name,
+      email: email,
+      password: password,
+      password_confirmation: password_confirmation,
+    };
+
+    const navigate = useNavigate();
+    axios
+      .post("http://127.0.0.1:8000/api/v1/users/register", user)
+      .then((response) => {
+        alert(response.data.message);
+        navigate("/login");
+      });
+  };
   return (
     <>
       <div className="flex justify-center items-center">
-        <h1 className="text-green-500">S'Enregistres</h1>
+        <h1 className="text-green-500">S'Enregistrer</h1>
         <div>
-          <form>
-            <div></div>
-            <button></button>
+          <form onSubmit={(e) => registerUser(e)} className="flex">
+            <div className="flex flex-col">
+              <label htmlFor="">
+                {" "}
+                Nom :
+                <input
+                  onChange={(e) => setName(e.target.value)}
+                  type="text"
+                  name="name"
+                />
+              </label>
+              <label htmlFor="">
+                {" "}
+                Email :
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  type="email"
+                  name="email"
+                />
+              </label>
+              <label htmlFor="">
+                {" "}
+                Mot de passe :
+                <input
+                  onChange={(e) => setPassword(e.target.value)}
+                  type="password"
+                  name="password"
+                />
+              </label>
+              <label htmlFor="">
+                {" "}
+                Confirmation Mot de passe :
+                <input
+                  onChange={(e) => setPasswordConfirmation(e.target.value)}
+                  type="password"
+                  name="password_confirmation"
+                />
+              </label>
+            </div>
+            <button type="submit">S'enregistrer</button>
           </form>
         </div>
       </div>
