04c110545a6bb320591d740fa759d59230efee07
SalahTazoukanit <salahtazu1996@gmail.com>
2024-09-05 19:19:48 +0200

    graphic component added with chart

--- frontend/src/components/doughnut/Graphic.jsx
+++ frontend/src/components/doughnut/Graphic.jsx
@@ -5,42 +5,56 @@ import { Doughnut } from "react-chartjs-2";
 
 const Graphic = () => {
   const [transactions, setTransactions] = useState([]);
-  const [incomings, setIncomings] = useState("");
-  const [outgo, setOutgo] = useState("");
-
+  const [incomings, setIncomings] = useState(0);
+  const [outgoings, setOutgoings] = useState(0);
+  const [difference, setDifference] = useState(0);
   const token = localStorage.getItem("token");
   const headers = {
     Authorization: "Bearer " + token,
   };
+
   const getTransactions = () => {
     axios
       .get("http://127.0.0.1:8000/api/v1/transactions", { headers })
       .then((response) => {
-        console.log(response.data.transactions);
-        setTransactions(response.data.transactions);
+        const transactionsData = response.data.transactions;
+        setTransactions(transactionsData);
 
-        // setIncomings(response.data.transactions);
+        let totalIncomings = 0;
+        let totalOutgoings = 0;
+
+        transactionsData.forEach((transaction) => {
+          if (transaction.type === "incomings") {
+            totalIncomings += transaction.amount;
+          } else if (transaction.type === "outgo") {
+            totalOutgoings += transaction.amount;
+          }
+        });
+
+        let difference = totalIncomings - totalOutgoings;
+
+        setIncomings(totalIncomings);
+        setOutgoings(totalOutgoings);
+        setDifference(difference);
       });
   };
-  
 
   useEffect(() => {
     getTransactions();
   }, []);
 
-  // data: transactions.map((transaction) => transaction.amount),
   return (
     <>
       <Doughnut
         data={{
-          labels: transactions.map((transaction) => transaction.label),
+          labels: ["Depenses", "Revenues", "Restant"],
           datasets: [
             {
-              label: "",
-              data: [300, 50, 100],
+              label: ["SmartWallet"],
+              data: [outgoings, incomings, difference],
               backgroundColor: [
                 "rgb(255, 99, 132)",
-                "rgb(54, 162, 235)",
+                "rgb(0, 128, 0)",
                 "rgb(255, 205, 86)",
               ],
               hoverOffset: 4,
